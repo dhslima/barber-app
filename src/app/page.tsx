14ea64e618e24b0ac6925db33456cc784229af10
David Lima <david.lima@ifal.edu.br>
2025-09-09 14:48:43 -0300

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import { Input } from "@/components/ui/input";
 import Header from "./_components/header";
+import { Button } from "@/components/ui/button";
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, David</h2>
+        <p>Quarta-feira, 10 de setembro de 2025</p>
+      </div>
+      <div className="mt-4 flex flex-row items-center gap-2">
+        <Input placeholder="Faça sua busca..." />
+        <Button>
+          <SearchIcon />
+        </Button>
+      </div>
+      <div className="relative mt-6 h-[150px] w-full">
+        <Image
+          src="/banner-01.png"
+          alt="Agende nos Melhores com FSW Barber"
+          fill
+          className="rounded-xl object-contain"
+        />
+      </div>
     </div>
   );
 }
